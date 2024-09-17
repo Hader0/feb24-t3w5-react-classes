aff004d25f64f40d165223ade0bc92b54f29cbc1
Hader0 <108206953+Hader0@users.noreply.github.com>
2024-09-17 15:16:37 +1000

    Componment didMount with multiple pokemon

--- reactjs-pokemon-class-demo/src/PokemonCard.jsx
+++ reactjs-pokemon-class-demo/src/PokemonCard.jsx
@@ -1,9 +1,13 @@
 
+// Function that capitalise's first letter of the Pokemon name
+function capitalizeFirstLetter(str) {
+    return `${str[0].toUpperCase()}${str.slice(1)}`;
+}
 
 export default function PokemonCard(props) {
     return (
         <h1>
-            {props.name}
+            {capitalizeFirstLetter(props.name)}
         </h1>
     )
 }
--- reactjs-pokemon-class-demo/src/PokemonFetcher.jsx
+++ reactjs-pokemon-class-demo/src/PokemonFetcher.jsx
@@ -8,9 +8,33 @@ export default class PokemonFetched extends React.Component {
         super(props);
 
         this.state = {
-            pokemonList: ["Pikachu", "Squirtle", "Mewtwo"]
+            pokemonList: []
         }
     }
+
+    // Runs automatically ONCE when the page first loads
+    async componentDidMount() {
+
+        for (let index = 0; index < 6; index++) {
+            // Generate a random number for random Pokemon number
+            let randomNumber = Math.floor(Math.random() * 1025) + 1;
+
+            // Pass random Pokemon number into fetch request
+            let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`);
+            let data = await response.json();
+
+            // Set fetch response data into state
+            // this.setState({pokemonList: [...this.state.pokemonList, data]});
+            this.setState((previousState) => {
+                return {
+                    pokemonList: [...previousState.pokemonList, data]
+                }
+            });
+        }
+
+        
+            console.log("PokemonFetcher first load on the page.");
+        }
 
     render() {
         return (
@@ -20,7 +44,7 @@ export default class PokemonFetched extends React.Component {
 
                 {/* Mapping the Pokemon list in the state */}
                 {this.state.pokemonList.map(pokemon => {
-                    return <PokemonCard name={pokemon}/>
+                    return <PokemonCard name={pokemon.name}/>
                 })}
 
                 {/* Button with onClick function for updating the state for the list */}
