010921e1978f7298f4fb407019b8a47bbdfa8ff7
Hader0 <108206953+Hader0@users.noreply.github.com>
2024-09-17 14:13:23 +1000

    Class and custom class and inheritance and static

--- jsclasses/app.js
+++ jsclasses/app.js
@@ -6,10 +6,56 @@ class Dog {
         this.collar = newCollar;
         this.age = newAge;
         this.cuteness = newCuteness;
+
+        Dog.numberOfDogs++;
+    }
+
+    // Private variable
+    static #numberOfDogs = 0;
+
+    // Getter
+    static get numberOfDogs() {
+        return Dog.#numberOfDogs;
+    }
+
+    // Setter
+    static set numberOfDogs(newValue) {
+        throw new NumberOfDogsError();
+    }
+
+    bark() {
+        console.log("Woof! I am " + this.name);
+        throw new BarkError(this.name, this.breed);
+    }
+}
+
+class NumberOfDogsError extends Error {
+    constructor() {
+        super("Cannot modify the number of dogs unless you're creating a dog via the Dog constructor");
     }
 }
 
 let fido = new Dog("Fido", "Frenchie", "Leather", 7);
 
-console.log(fido);
-console.log(fido.cuteness);
+console.log(Dog.numberOfDogs);
+
+// console.log(fido);
+// console.log(fido.cuteness);
+
+// fido.bark();
+
+
+class BarkError extends Error {
+    constructor(dogName, dogBreed) {
+        super(`${dogName} of breed ${dogBreed} failed to bark.`);
+    }
+}
+
+try {
+    fido.bark();
+} catch (error) {
+    // throw new BarkError(fido.name, fido.breed);
+    if (error instanceof BarkError) {
+        console.log("Bark error occured.")
+    }
+}
